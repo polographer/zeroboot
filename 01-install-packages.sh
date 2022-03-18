d83ef17df89ca37be4f739eb40907197252fec34
Polo Ornelas <m@poloornelas.mx>
2022-03-18 08:53:00 -0400

    adding mosh to the initial set of packages

--- 01-install-packages.sh
+++ 01-install-packages.sh
@@ -1,2 +1,2 @@
-sudo apt install -y wget gnupg2 vim nmap imagemagick links libpq-dev screen neofetch
+sudo apt install -y wget gnupg2 vim nmap imagemagick links libpq-dev screen neofetch mosh
 echo "shell -$SHELL" > ~/.screenrc
